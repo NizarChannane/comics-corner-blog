1ca7dd6cbf8f0e314d0008ad02db21bb44387c88
unknown <nizar.channane@gmail.com>
2024-01-20 03:26:48 +0100

    Add environment variables throughout the code base

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "")
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "")
+        }
       }
-    }
-  },
-  plugins: [react()],
-  assetsInclude: ['**/*.md']
-})
+    },
+    plugins: [react()],
+    assetsInclude: ['**/*.md']
+  }
+});
--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const config = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "test_comicscorner",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_DATABASE,
 };
 
 export const query = async (sql, params) => {
